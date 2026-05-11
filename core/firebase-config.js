33388ced2045386560c6b70c00d9d2805846e9d9
mtreck <mateus8reck@hotmail.com>
2026-05-11 17:23:01 -0300

    fix: re-adiciona firebase-config.js no github para o Vercel conseguir servir as chaves publicas do front-end

---
+++ core/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDjLbDsSz5YJFKCpxUELRu83n9UXY4WiYw",
+  authDomain: "orbita-fatecivp.firebaseapp.com",
+  projectId: "orbita-fatecivp",
+  storageBucket: "orbita-fatecivp.firebasestorage.app",
+  messagingSenderId: "799058002908",
+  appId: "1:799058002908:web:bddfcb8d7e7c0f7590c519",
+  measurementId: "G-MZN1E8RGDC"
+};
